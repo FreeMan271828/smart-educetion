a37a813d092fe51316db27a3b029c324a57c8455
wind <wind_station@qq.com>
2025-05-15 16:08:27 +0800

    fix: 修正跨域配置，放行服务器IP的所有端口

--- common/src/main/java/org/nuist/config/security/CommonSecurityConfigurer.java
+++ common/src/main/java/org/nuist/config/security/CommonSecurityConfigurer.java
@@ -28,7 +28,8 @@ public class CommonSecurityConfigurer extends AbstractHttpConfigurer<CommonSecur
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("**"));
+//        configuration.setAllowedOrigins(List.of("**"));
+        configuration.setAllowedOriginPatterns(List.of("http://118.89.136.119:[*]", "https://118.89.136.119:[*]"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
